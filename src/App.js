9b823d0bcff244ceb1334545a38b95d85390c2fa
Razan Armouti <razanarmote@yahoo.com>
2021-09-13 01:05:56 +0300

    map done

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+
 export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       city_name: "",
-    
+      MapLoc:"",
       lat: "",
       lon: "",
       showData: false,
-      showModal: false
+      showModal: false,
+      zoom:0
     }
   }
   handleClose = () => {
@@ -32,23 +34,50 @@ export class App extends Component {
     }
     handleSubmit = (e) => {
       e.preventDefault();
-      axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city_name}&format=json`).then((response) => {
-        console.log('Everything is awesome.');
+      axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}
+      &q=${this.state.city_name}&format=json`).then((response) => {
+       
+      console.log('city location - Everything is awesome.');
         let responseData = response.data[0]
         this.setState({
           city_name: responseData.display_name,
           lon: responseData.lon,
           lat: responseData.lat,
-          
+          MapLoc: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${responseData.lat},${responseData.lon}&zoom=10`,
           showData: true,
           showModal: true
 
         })
+        console.log(this.state.MapLoc);
       }).catch((error) => {
-        console.warn('Not good man :(');
+        console.warn('city location - Not good man :(');
       })
 
+     
+      
+
     }
+    // handleMap = (e) => {
+    //   e.preventDefault();
+    //   axios.get(`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}
+    //   &center=${this.state.lat},${this.state.lon}&zoom=10`).then((res)=>{
+    //     console.log('inside map- Everything is awesome.');
+    //     let responseData = res.data[0]
+    //     this.setState({
+    //       city_name: responseData.display_name,
+    //       lon: responseData.lon,
+    //       lat: responseData.lat,
+    //       zoom:13,
+    //       showData: true,
+    //       showModal: true
+
+    //     })
+
+    //   }).catch((error)=>{
+    //     console.warn('inside map- Not good man :(');
+    //   })
+
+    // }
 
     render(){
       return (
@@ -60,10 +89,14 @@ export class App extends Component {
             <Location handleClose={this.handleClose} 
                       showModal={this.state.showModal} 
                       city_name={this.state.city_name}
-                      
+                      MapLoc={this.state.MapLoc}
                       lat={this.state.lat}
                       lon={this.state.lon}/>
+           
+            
           }
+          
+                
 
         </div>
       )
