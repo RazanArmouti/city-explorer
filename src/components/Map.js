d60e6ec85d1bc57b20f1005dc7c14ed6a66cf953
Razan Armouti <razanarmote@yahoo.com>
2021-09-13 01:09:46 +0300

    solved error

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,7 +8,7 @@ export class Map extends Component {
                     <Card.Title> <h1>{this.props.city_name}</h1></Card.Title>
                     <Card.Subtitle className="mb-2 text-muted"> <h3>{this.props.lat}/{this.props.lon}</h3></Card.Subtitle>
                     <Card.Text>
-                        <iframe src={this.props.MapLoc} style={{ width: '600px', height:'400px' } }>
+                        <iframe title="mapIframe" src={this.props.MapLoc} style={{ width: '600px', height:'400px' } }>
                         </iframe>
                     </Card.Text>
 
